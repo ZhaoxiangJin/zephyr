6b19fa05ee544046d098cedc3b7a5dbc8b903986
Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>
2026-04-03 15:43:40 +0800

    clock_control: mcux_syscon: add RT7xx IOCON gates
    Add RT7xx IOCON clock IDs to the MCUX SYSCON clock bindings and
    handle them in the shared clock control driver.
    
    This provides the common gate support needed for RT7xx IOCON
    instances to be managed through the clock control API.

--- drivers/clock_control/clock_control_mcux_syscon.c
+++ drivers/clock_control/clock_control_mcux_syscon.c
@@ -18,6 +18,21 @@ LOG_MODULE_REGISTER(clock_control);
 static int mcux_lpc_syscon_clock_control_on(const struct device *dev,
 					    clock_control_subsys_t sub_system)
 {
+#if defined(CONFIG_SOC_SERIES_IMXRT7XX)
+	switch ((uint32_t)sub_system) {
+	case MCUX_IOCON0_CLK:
+		CLOCK_EnableClock(kCLOCK_Iopctl0);
+		break;
+	case MCUX_IOCON1_CLK:
+		CLOCK_EnableClock(kCLOCK_Iopctl1);
+		break;
+	case MCUX_IOCON2_CLK:
+		CLOCK_EnableClock(kCLOCK_Iopctl2);
+		break;
+	default:
+		break;
+	}
+#endif
 #if defined(CONFIG_CAN_NXP_LPC_MCAN)
 	if ((uint32_t)sub_system == MCUX_MCAN_CLK) {
 		CLOCK_EnableClock(kCLOCK_Mcan);
